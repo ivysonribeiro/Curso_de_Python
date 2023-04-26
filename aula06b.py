71fb49acfa927ad473ae6fb3c513aba80f71a242
Ivyson Ribeiro <ribeiroivyson@yahoo.com>
2023-04-26 16:09:13 -0300

    ex003

--- aula06b.py
+++ aula06b.py
@@ -2,3 +2,6 @@ n = input('Digite um valor: ')
 #print(n.isnumeric()) ...número
 #print(n.isalpha())  ...letra
 #print(n.isalnum()) ...alfanumérico
+print(type(n.isnumeric()))
+print(type(n.isalpha()))
+print(type(n.isalnum()))
